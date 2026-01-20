c46305bc0cbab7f6fbda56f964578437a24fff4e
recently_was_good <ustinovlev039@gmail.com>
2026-01-20 22:12:58 +1000

    Добивочка

--- src/shop.py
+++ src/shop.py
@@ -1,12 +1,13 @@
-
-
 class Product:
 
-    def __init__(self, product_name: str, product_description: str, price: int, quantity: int):
+    def __init__(
+        self, product_name: str, product_description: str, price: int, quantity: int
+    ):
         self.product_name = product_name
         self.product_description = product_description
         self.price = price
         self.quantity = quantity
+
 
 class Category:
 
@@ -21,23 +22,5 @@ class Category:
         Category.count_product += len(product)
 
         Category.count_category += 1
-
-
-if __name__ == "__main__":
-    prod1 = Product("LG", "Телевизор", 50_000, 5)
-    prod2 = Product("Samsung", "Монитор", 100_000, 15)
-    prod3 = Product("Apple", "Ipad", 78_000, 45)
-
-    cat1 = Category("Электроника", "Техника для дома", [prod1, prod2])
-
-    print(Category.count_category)
-    print(Category.count_product)
-
-    cat2 = Category("Телефоны", "Личного пользования", [prod3])
-
-    print()
-    print(Category.count_category)
-    print(Category.count_product)
-
 
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,26 +1,32 @@
 import pytest
-from src.shop import Product
-from src.shop import Category
+
+from src.shop import Category, Product
+
 
 @pytest.fixture
 def product1():
     return Product("LG", "Телевизор", 50_000, 5)
 
+
 @pytest.fixture
 def product2():
     return Product("Samsung", "Монитор", 100_000, 15)
+
 
 @pytest.fixture
 def product3():
     return Product("Apple", "Ipad", 78_000, 45)
 
+
 @pytest.fixture
 def category1(product1, product2):
     return Category("Электроника", "Техника для дома", [product1, product2])
 
+
 @pytest.fixture
 def category2(product3):
     return Category("Телефоны", "Личного пользования", [product3])
+
 
 @pytest.fixture(autouse=True)
 def reset_count():
